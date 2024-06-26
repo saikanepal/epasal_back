af0bc699fdf5bea151443f19a6920bad4dae600f
Sameer <nischalkarn369369@gmail.com>
2024-06-27 01:06:27 +0300

    Remove AWS keys from EmailConfig.js

---
+++ Email-Config-SES-AWS/EmailConfig.js
@@ -0,0 +1,14 @@
+
+// DONT USE THESE ONLY FOR DEVELOPMENT
+const accesskey = ``;
+const secretaccesskey = ``;
+const sender = '';
+const region = '';
+
+module.exports = {
+    accesskey,
+    secretaccesskey,
+    sender,
+    region
+};
+
